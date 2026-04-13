e24af0ab0d3a91c29396c265d29ffbc1172bb65b
amplob <marc.planas@estudiantat.upc.edu>
2026-04-13 12:05:39 +0200

    Configuro Firebase Realtime Database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   // Pon aqui la URL base de Firebase Realtime Database, sin slash final.
   // Ejemplo: https://mi-carrera-default-rtdb.europe-west1.firebasedatabase.app
-  remoteDbUrl: "",
+  remoteDbUrl: "https://apuestas-fed65-default-rtdb.europe-west1.firebasedatabase.app",
 };
